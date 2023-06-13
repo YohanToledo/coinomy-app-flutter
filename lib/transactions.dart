4e530dae055cc9a7d35e339068e39f2dfd193a42
YOHAN GABRIEL TOLEDO BERNART <YGTBERNART@UNIVEL.UNIVEL.BR>
2023-06-12 21:19:00 -0300

    add select and button to transaction screen

--- lib/transactions.dart
+++ lib/transactions.dart
@@ -6,6 +6,13 @@ import 'package:intl/intl.dart';
 import 'global-constants.dart';
 import 'login.dart';
 
+const List<String> list = <String>[
+  'Mercado',
+  'Salario',
+  'Rendimento',
+  'Combustivel'
+];
+
 class Transactions extends StatefulWidget {
   @override
   State<Transactions> createState() => _Transactions();
@@ -88,7 +95,16 @@ class _Transactions extends State<Transactions> {
                       print("Date is not selected");
                     }
                   },
-                )
+                ),
+                descField(),
+                SizedBox(
+                  height: 10,
+                ),
+                DropdownButtonField(),
+                SizedBox(
+                  height: 10,
+                ),
+                saveButton(),
               ]),
             ),
           ])),
@@ -123,64 +139,83 @@ Widget valueField() {
       ));
 }
 
-Widget dateSelect() {
-  return TextField();
+Widget descField() {
+  return TextFormField(
+      keyboardType: TextInputType.text,
+      style: TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+          color: LIGHT_COLOR,
+        )),
+        label: Text(
+          "Descrição",
+          style: TextStyle(color: LIGHT_COLOR),
+        ),
+        icon: Icon(
+          Icons.calculate,
+          color: LIGHT_COLOR,
+        ),
+      ));
 }
 
-class HomePage extends StatefulWidget {
+class DropdownButtonField extends StatefulWidget {
+  const DropdownButtonField({super.key});
+
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<DropdownButtonField> createState() => _DropdownButtonState();
 }
 
-class _HomePageState extends State<HomePage> {
-  TextEditingController dateController = TextEditingController();
-
-  @override
-  void initState() {
-    dateController.text = ""; //set the initial value of text field
-    super.initState();
-  }
+class _DropdownButtonState extends State<DropdownButtonField> {
+  String dropdownValue = list.first;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text("Datepicker")),
-        body: Container(
-            padding: const EdgeInsets.all(15),
-            height: 150,
-            child: Center(
-                child: TextField(
-              controller: dateController, //editing controller of this TextField
-              decoration: const InputDecoration(
-                  icon: Icon(Icons.calendar_today), //icon of text field
-                  labelText: "Enter Date" //label text of field
-                  ),
-              readOnly: true, // when true user cannot edit text
-              onTap: () async {
-                DateTime? pickedDate = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(), //get today's date
-                    firstDate: DateTime(
-                        2000), //DateTime.now() - not to allow to choose before today.
-                    lastDate: DateTime(2101));
-
-                if (pickedDate != null) {
-                  print(
-                      pickedDate); //get the picked date in the format => 2022-07-04 00:00:00.000
-                  String formattedDate = DateFormat('yyyy-MM-dd').format(
-                      pickedDate); // format date in required form here we use yyyy-MM-dd that means time is removed
-                  print(
-                      formattedDate); //formatted date output using intl package =>  2022-07-04
-                  //You can format date as per your need
-
-                  setState(() {
-                    dateController.text =
-                        formattedDate; //set foratted date to TextField value.
-                  });
-                } else {
-                  print("Date is not selected");
-                }
-              },
-            ))));
+    return DropdownButton<String>(
+      alignment: Alignment.bottomLeft,
+      dropdownColor: DARK_COLOR,
+      value: dropdownValue,
+      icon: const Icon(
+        Icons.arrow_downward,
+        color: LIGHT_COLOR,
+      ),
+      underline: Container(
+        height: 0,
+      ),
+      elevation: 16,
+      style: const TextStyle(color: LIGHT_COLOR),
+      onChanged: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      items: list.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
   }
+}
+
+Widget saveButton() {
+  return Container(
+      height: 40,
+      width: 100,
+      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+      child: ElevatedButton(
+        child: const Text(
+          'Salvar',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.green, elevation: 5),
+        onPressed: () {},
+      ));
 }
